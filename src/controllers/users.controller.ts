561aecea87477e58363e5409e80ba57bf4cba20f
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-21 14:10:02 -0300

    melhorias no controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,23 +5,62 @@ export class UsersController {
   constructor(private readonly usersServices: UsersServices) {}
 
   async createUser(req: Request, res: Response): Promise<Response> {
-    const newUser = await this.usersServices.createUser(req.body);
+    try {
+      const newUser = await this.usersServices.createUser(req.body);
 
-    return res
-      .status(201)
-      .json({ mensagem: "Usuario criado com sucess", usuario: newUser });
+      return res
+        .status(201)
+        .json({ mensagem: "Usuario criado com sucess", usuario: newUser });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "erro interno no servidor" });
+    }
   }
 
   async findUser(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-    const foundUser = await this.usersServices.findUser(id);
+    try {
+      const { id } = req.params;
+      const foundUser = await this.usersServices.findUser(id);
 
-    return res.status(201).json({ usuario: foundUser });
+      return res.status(201).json({ usuario: foundUser });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "erro interno no servidor" });
+    }
   }
 
   async findAllUsers(req: Request, res: Response): Promise<Response> {
-    const listUsers = await this.usersServices.findAllUsers();
+    try {
+      const listUsers = await this.usersServices.findAllUsers();
 
-    return res.status(200).json({ usuarios: listUsers });
+      return res.status(200).json({ usuarios: listUsers });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "erro interno no servidor" });
+    }
+  }
+
+  async updateUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const updatedUser = await this.usersServices.updateUser(id, req.body);
+
+      return res.status(200).json({ updatedUser });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "erro interno no servidor" });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.body;
+      await this.usersServices.deleteUser(id);
+
+      return res.status(200).json({ mensagem: "Usuario deletado com sucesso" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "erro interno no servidor" });
+    }
   }
 }
