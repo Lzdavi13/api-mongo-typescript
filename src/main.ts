6b2bd07fed8aa84c90f84fc1c726490dbd4546ae
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-21 10:12:48 -0300

    conexao com o mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import { database } from "./database/connection";
+import { userController } from "./factory/users.controller.factory";
 
 const app = express();
-
 app.use(express.json());
+
+database._connect();
+
+app.post("/user", (request, response) => {
+  return userController.createUser(request, response);
+});
+
+app.get("/user/:id", (request, response) => {
+  return userController.findUser(request, response);
+});
 
 app.listen(3333, () => {
   console.log("servidor iniciado");
