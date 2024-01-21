06e0107373e6217356570a93762cfac08acea6d8
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-21 16:14:00 -0300

    dotenv adicionado ao projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import { database } from "./database/connection";
+import userLoginController from "./factory/user.login.controller.factory";
 import { userController } from "./factory/users.controller.factory";
+import { ensureAuthenticated } from "./middleware/EnsureAuthenticated";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +13,12 @@ database._connect();
 app.post("/user", (request, response) => {
   return userController.createUser(request, response);
 });
+
+app.post("user/login", (request, response) => {
+  return userLoginController.handle(request, response);
+});
+
+app.use(ensureAuthenticated);
 
 app.get("/user/:id", (request, response) => {
   return userController.findUser(request, response);
